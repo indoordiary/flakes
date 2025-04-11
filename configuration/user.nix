a47ab352ff94179e0e57b69bbe70803e08c9a854
indoordiary <lfn2333333@gmail.com>
2025-04-12 01:53:31 +0800

    Update user.nix

--- configuration/user.nix
+++ configuration/user.nix
@@ -7,7 +7,7 @@
       isNormalUser = true;
       extraGroups = [ "wheel" "audio" "video" "docker" ];
       useDefaultShell = true;
-      hashedPassword = "$y$j9T$n/ymRgCEJ.hDWTIbs1zgy.$DScp0RP5zKv4AZJfEKy5nxQQqL0JZkoBHccbTh5xco1";
+      initialPassword = "FFFF";
     };
   };
 
