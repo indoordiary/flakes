75b85a79bee63eea67fe4bb6fcd6888f300ed603
indoordiary <lfn2333333@gmail.com>
2025-04-13 04:58:03 +0800

    Update install-system.nix

--- configuration/programs/install-system.nix
+++ configuration/programs/install-system.nix
@@ -2,26 +2,11 @@
 {
   environment.systemPackages = with pkgs; [
 
-    # Desktop
-    mako tofi waybar alacritty
-
-    # Utils
+    #Utils
     git fastfetch xorg.xrandr wl-clipboard wlr-randr
 
     # Tools
     unzip unrar p7zip sof-firmware
-
-    # Social 
-    qq wechat-uos telegra-desktop 
-
-    # dev
-    vscode typora
-
-    # Entertainment
-    bilibili qqmusic
-
-    # inpus
-    inputs.zen-browser.packages."${system}".beta
     
   ];
 }
