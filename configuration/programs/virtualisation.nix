5250b52c89d32304b8d0faf5ddd7ef425f7bd3ef
indoordiary <lfn2333333@gmail.com>
2025-04-13 03:56:17 +0800

    Update virtualisation.nix

--- configuration/programs/virtualisation.nix
+++ configuration/programs/virtualisation.nix
@@ -1,11 +1,20 @@
-{ lib, config, ... }:
-
-lib.mkIf config.optional.podman {
-
-  virtualisation = {
-    podman = {
-      enable = true;
-      dockerCompat = true;
+{ config, pkgs, lib, ... }:
+{
+  virtualisation.podman = {
+    enable = true;
+    dockerCompat = true;
+    dockerSocket.enable = true;
+    defaultNetwork.settings = {
+      dns_enabled = true;
     };
   };
+
+  environment.systemPackages = with pkgs; [
+    podman-compose
+    lazydocker
+  ];
+
+  virtualisation.waydroid.enable = true;
+
+  boot.kernel.sysctl."user.max_user_namespaces" = 16384;
 }
