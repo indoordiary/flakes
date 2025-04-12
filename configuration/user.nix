244ded4d32435361a30cc3bba41544a8194c56a3
indoordiary <lfn2333333@gmail.com>
2025-04-13 05:27:26 +0800

    Update user.nix

--- configuration/user.nix
+++ configuration/user.nix
@@ -5,7 +5,7 @@
     mutableUsers = false;
     users.chenhsi = {
       isNormalUser = true;
-      extraGroups = [ "wheel" "audio" "video" "network-manager" "docker" ];
+      extraGroups = [ "wheel" "audio" "video" "network-manager" "podman" ];
       useDefaultShell = true;
       hashedPassword = "$6$8/ZJ0jr/BWf4at92$PmRuEML2eiCHcRe/vuc54xgSkU.T7bO6ljUqczhkpw3kAv1mTl.PLMOhRuG.yIiEklJGwcHq3szit4PPkZaEM0";
     };
