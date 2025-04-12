aee9a9ed0f009f5d5c73ff3234e21ad9764183ac
indoordiary <lfn2333333@gmail.com>
2025-04-13 04:54:40 +0800

    Update hyprland.nix

--- configuration/wm/hyprland.nix
+++ configuration/wm/hyprland.nix
@@ -10,22 +10,6 @@ imports = [
     enable = true;
     # xwayland.enable = true;
   };
-  
-  services.hypridle = {
-    enable = true;
-    settings = {
-      general = {
-        before_sleep_cmd = "${pkgs.systemd}/bin/loginctl lock-session";
-        lock_cmd = "${lib.getExe config.programs.hyprlock.package}";
-      };
-      listener = [
-        {
-          timeout = 300;
-          on-timeout = "${pkgs.systemd}/bin/systemctl suspend";
-        }
-      ];
-    };
-  };
 
   programs.hyprlock.enable = true;
 }
