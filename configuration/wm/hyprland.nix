993de223c03f7eeeeba02f5cfc9e59856aef7a4e
indoordiary <lfn2333333@gmail.com>
2025-04-13 04:47:34 +0800

    Create hyprland.nix

---
+++ configuration/wm/hyprland.nix
@@ -0,0 +1,26 @@
+{ pkgs, lib, config, ... }:
+
+{
+  programs.hyprland = {
+    enable = true;
+    # xwayland.enable = true;
+  };
+  
+  services.hypridle = {
+    enable = true;
+    settings = {
+      general = {
+        before_sleep_cmd = "${pkgs.systemd}/bin/loginctl lock-session";
+        lock_cmd = "${lib.getExe config.programs.hyprlock.package}";
+      };
+      listener = [
+        {
+          timeout = 300;
+          on-timeout = "${pkgs.systemd}/bin/systemctl suspend";
+        }
+      ];
+    };
+  };
+
+  programs.hyprlock.enable = true;
+}
