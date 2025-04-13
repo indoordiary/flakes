79135dac38e02291ad1e85153e51027ede834c08
indoordiary <lfn2333333@gmail.com>
2025-04-13 19:44:26 +0800

    Update default.nix

--- configuration/wm/input/fcitx5/default.nix
+++ configuration/wm/input/fcitx5/default.nix
@@ -1,18 +1,13 @@
-{ config, ... }:
-let
-  fcitx5Home = "${config.xdg.configHome}/fcitx5";
-in
+ { config, pkgs, ... }:
 {
-  home.file = {
-    "${fcitx5Home}/profile".source = ./profile;
-    "${fcitx5Home}/config".source = ./config;
-    "${fcitx5Home}/conf/classicui.conf".source = ./classicui.conf;
-    "${fcitx5Home}/conf/rime.conf".source = ./rime.conf;
-    ".local/share/fcitx5/rime/default.custom.yaml".source = ./default.custom.yaml;
+  i18n.inputMethod = {
+    enable = true;
+    type = "fcitx5";
+    fcitx5 = {
+      addons = with pkgs; [
+        fcitx5-rime fcitx5-pinyin-moegirl
+      ];
+      waylandFrontend = true;
+    };
   };
-
-  # i18n.inputMethod.fcitx5.catppuccin = {
-  #   enable = true;
-  #   apply = true;
-  # };
 }
