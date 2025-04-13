0faa2b78619e3d03c950a56d88714c656e711e01
Chenhsi Li <2244193927@qq.com>
2025-04-13 11:02:29 +0800

    .

--- configuration/programs/grub.nix
+++ configuration/programs/grub.nix
@@ -8,17 +8,8 @@
         device = "nodev";
         useOSProber = true;
         efiSupport = true;
-        theme = pkgs.catppuccin-grub.override{
-          flavor = "mocha";
-        };
-<<<<<<< HEAD
-        gfxmodeEfi = "1024x768";
-=======
-        extraConfig = ''
-          GRUB_GFXMODE=1024x768
-          GRUB_GFXPAYLOAD_LINUX=keep
-        '';
->>>>>>> 0db983b3e2afd7f507b7af04bce32ebda9215f3b
+      # theme = pkgs.catppuccin-grub.override { flavor = "mocha"; };
+      # gfxmodeEfi = "1024x768";
      };
   };
 }
--- configuration/wm/hyprland.nix
+++ configuration/wm/hyprland.nix
@@ -1,10 +1,43 @@
-{ ... }:
+{ config, pkgs,... }:
 
 {
   imports = [
     ./input/fcitx5/default.nix
   ];
+
   environment.systemPackages = with pkgs; [
-    catppuccin waybar mako
+      kitty
+      dunst
+      waybar
+      brightnessctl
+      pavucontrol
+      pipewire
+      pipewire.pulse
+      pipewire.alsa
+      wireplumber
+      hyprpaper
+      hyprlock
+      hyprpicker
+      grim
+      slurp
+      wofi
+      cliphist
+      xfce.thunar
+      gvfs
+      gtk4
+      seahorse
+      gnome.gnome-keyrings
+      libsForQt5.qt5ct
+      qt6.qtwayland
+      libsForQt5.qtwayland
+      xdg-desktop-portal-gtk
+      uwsm
+      hyprpolkitagent
+      xdg-desktop-portal-hyprland
   ];
+
+  programs.hyprland.enable = true;
+  environment.sessionVariables.NIXOS_OZONE_WL = "1";
+  environment.sessionVariables.WLR_NO_HARDWARE_CURSORS = "1";
+  services.xserver.displayManager.gdm.enable = true; 
 }
