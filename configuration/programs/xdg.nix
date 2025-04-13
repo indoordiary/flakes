cd981dbf63ac7acbb2493b559e195455c15047dd
indoordiary <lfn2333333@gmail.com>
2025-04-13 15:10:18 +0800

    Create xdg.nix

---
+++ configuration/programs/xdg.nix
@@ -0,0 +1,19 @@
+{ config, pkgs, ... }:
+
+{
+  xdg.portal = {
+    enable = true;
+    hyprland.enable = true; 
+
+    extraPortals = with pkgs; [
+      xdg-desktop-portal-hyprland
+      xdg-desktop-portal-gtk
+    ];
+  };
+
+  environment.systemPackages = with pkgs; [
+    xdg-desktop-portal-hyprland
+    xdg-desktop-portal-gtk
+    xdg-desktop-portal-wlr
+  ];
+}
