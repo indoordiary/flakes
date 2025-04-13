5646840938f215e1e8b7d8e3b2b36050e7c5d4ae
indoordiary <lfn2333333@gmail.com>
2025-04-13 12:09:17 +0800

    Update user.nix

--- configuration/user.nix
+++ configuration/user.nix
@@ -1,6 +1,9 @@
 { config, pkgs, ... }:
 
 {
+  users.users.root = {
+    initialhashedHashedPassword = "$6$8/ZJ0jr/BWf4at92$PmRuEML2eiCHcRe/vuc54xgSkU.T7bO6ljUqczhkpw3kAv1mTl.PLMOhRuG.yIiEklJGwcHq3szit4PPkZaEM0";
+  };
   users = {
     mutableUsers = false;
     users.chenhsi = {
