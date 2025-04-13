d73605520a762f36ba6a4e7d7d04f1de4f0fd8a3
indoordiary <lfn2333333@gmail.com>
2025-04-13 15:40:32 +0800

    Update programs.nix

--- configuration/programs.nix
+++ configuration/programs.nix
@@ -19,6 +19,12 @@
     ./programs/virtualisation.nix
   ];
 
+  programs.hyprland.enable = true;
+  environment.sessionVariables.NIXOS_OZONE_WL = "1";
+  environment.sessionVariables.WLR_NO_HARDWARE_CURSORS = "1";
+  services.xserver.displayManager.gdm.enable = true; 
+
+
   nixpkgs.config.permittedInsecurePackages = [
 
   ];
