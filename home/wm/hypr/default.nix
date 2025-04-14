096138723a9f5331082b69bbbf8c2f671d4b6efb
indoordiary <lfn2333333@gmail.com>
2025-04-14 21:11:29 +0800

    Update default.nix

--- home/wm/hypr/default.nix
+++ home/wm/hypr/default.nix
@@ -7,16 +7,15 @@
   ];
 
   home.sessionVariables = {
-    "NIXOS_OZONE_WL,1"
-    "MOZ_ENABLE_WAYLAND,1"
-    "MOZ_WEBRENDER,1"
-
-    "_JAVA_AWT_WM_NONREPARENTING,1"
-    "QT_WAYLAND_DISABLE_WINDOWDECORATION,1"
-    "QT_QPA_PLATFORM,wayland"
-    "SDL_VIDEODRIVER,wayland"
-    "GDK_BACKEND,wayland"
-    };
+    NIXOS_OZONE_WL = "1";
+    MOZ_ENABLE_WAYLAND = "1";
+    MOZ_WEBRENDER = "1";
+    _JAVA_AWT_WM_NONREPARENTING = "1";
+    QT_WAYLAND_DISABLE_WINDOWDECORATION = "1";
+    QT_QPA_PLATFORM = "wayland";
+    SDL_VIDEODRIVER = "wayland";
+    GDK_BACKEND = "wayland";
+  };
 
   extraConfig = builtins.readFile ./conf/hyprland.conf;    
 
