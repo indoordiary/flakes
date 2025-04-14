219d209b4749dddcc398714061b1a68f33392afe
indoordiary <lfn2333333@gmail.com>
2025-04-14 21:41:00 +0800

    Update others.nix

--- programs/others.nix
+++ programs/others.nix
@@ -18,9 +18,5 @@
 
   virtualisation.waydroid.enable = true;
 
-  environment.sessionVariables.NIXOS_OZONE_WL = "1";
-  environment.sessionVariables.WLR_NO_HARDWARE_CURSORS = "1";
-  services.xserver.displayManager.gdm.enable = true;  
-
   documentation.nixos.enable = false;
 }
